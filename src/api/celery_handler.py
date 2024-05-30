9ec499bbea5c5c2082965b33589796196de38022
mkollo <marko.kollo96@gmail.com>
2024-05-30 16:16:42 +0300

    Initial structure and setup.

---
+++ src/api/celery_handler.py
@@ -0,0 +1,16 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+app = Celery('taskman')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
