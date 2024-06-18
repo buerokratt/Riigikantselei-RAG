62dcaf79c34786d08019bac06a2e45f5d7fa2393
mkollo <marko.kollo96@gmail.com>
2024-06-18 15:35:51 +0300

    Changed format of initial login.

--- src/user_profile/tests/test_get_token.py
+++ src/user_profile/tests/test_get_token.py
@@ -1,6 +1,4 @@
-from base64 import b64encode
-
-from rest_framework import HTTP_HEADER_ENCODING, status
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -10,8 +8,10 @@ from user_profile.utilities import create_test_user_with_user_profile
 
 class TestGetToken(APITestCase):
     def setUp(self) -> None:  # pylint: disable=invalid-name
+        self.username = 'manager'
+        self.password = 'password'
         self.manager_auth_user = create_test_user_with_user_profile(
-            self, 'manager', 'manager@email.com', 'password', is_manager=True
+            self, self.username, 'manager@email.com', self.password, is_manager=True
         )
         self.token_endpoint_url = reverse('get_token')
 
@@ -19,23 +19,18 @@ class TestGetToken(APITestCase):
         manager_token, created = Token.objects.get_or_create(user=self.manager_auth_user)
         self.assertTrue(created)
 
-        encoded_credentials = b64encode('manager:password'.encode(HTTP_HEADER_ENCODING)).decode(
-            HTTP_HEADER_ENCODING
+        response = self.client.post(
+            self.token_endpoint_url,
+            data={"username": self.username, "password": self.password}
         )
-        self.client.credentials(HTTP_AUTHORIZATION=f'Basic {encoded_credentials}')
-
-        response = self.client.get(self.token_endpoint_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
         self.assertEqual(response.data['token'], manager_token.key)
 
     def test_get_new_token(self) -> None:
-        encoded_credentials = b64encode('manager:password'.encode(HTTP_HEADER_ENCODING)).decode(
-            HTTP_HEADER_ENCODING
+        response = self.client.post(
+            self.token_endpoint_url,
+            data={"username": self.username, "password": self.password}
         )
-        self.client.credentials(HTTP_AUTHORIZATION=f'Basic {encoded_credentials}')
-
-        response = self.client.get(self.token_endpoint_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         manager_token, created = Token.objects.get_or_create(user=self.manager_auth_user)
